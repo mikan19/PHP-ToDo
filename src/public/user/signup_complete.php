e0923d2d460f921ab9656040c08a17ad5f8367ca
KatsuyaTanaka95 <gu0520kt@gmail.com>
2023-06-14 14:55:41 +0900

    refactor: ユーザー登録機能のリファクタ

--- src/public/user/signup_complete.php
+++ src/public/user/signup_complete.php
@@ -1,68 +1,49 @@
 <?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$passcheck = $_POST["passcheck"];
+
 session_start();
+if (empty($email) || empty($password)) {
+    $_SESSION["error_message"] = "EmailかPasswordの入力がありません";
+    header("Location: signup.php");
+    exit;
+} 
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $passcheck = $_POST["passcheck"];
-
-    if (empty($email) || empty($password)) {
-        $_SESSION["error_message"] = "EmailかPasswordの入力がありません";
-        header("Location: signup.php");
-        exit;
-    } elseif ($password !== $passcheck) {
-        $_SESSION["error_message"] = "パスワードが一致しません";
-        header("Location: signup.php");
-        exit;
-    } else {
-        $dbUserName = 'root';
-        $dbPassword = 'password';
-        $pdo = new PDO(
-            'mysql:host=mysql; dbname=todo; charset=utf8',
-            $dbUserName,
-            $dbPassword
-        );
-
-        try {
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // パスワードをハッシュ化
-
-            $stmt = $pdo->prepare("INSERT INTO users (
-                name, email, password
-            ) VALUES (
-                :name, :email, :password
-            )");
-        
-            $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-            $stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR); // ハッシュ化されたパスワードをバインド
-            $res = $stmt->execute();
-
-            // 登録成功時の処理
-            if ($res) {
-                // 例として、登録完了後にユーザーをログインページにリダイレクトする
-                header("Location: signin.php");
-                exit;
-            } else {
-                $_SESSION["error_message"] = "エラーが発生しました。もう一度お試しください。";
-                header("Location: signup.php");
-                exit;
-            }
-        } catch (PDOException $e) {
-            if ($e->getCode() == 23000) {
-                $_SESSION["error_message"] = "すでに保存されているメールアドレスです";
-            } else {
-                $_SESSION["error_message"] = "エラーが発生しました。もう一度お試しください。";
-            }
-            header("Location: signup.php");
-            exit;
-        }
-    }
-} else {
+if ($password !== $passcheck) {
+    $_SESSION["error_message"] = "パスワードが一致しません";
     header("Location: signup.php");
     exit;
 }
-?>
 
+$dbUserName = 'root';
+$dbPassword = 'password';
+$pdo = new PDO(
+    'mysql:host=mysql; dbname=todo; charset=utf8',
+    $dbUserName,
+    $dbPassword
+);
 
+$sqlSerectUsersByEmail = 'select * from users where email = :email';
+$statement = $pdo->prepare($sqlSerectUsersByEmail);
+$statement->bindValue(':email', $email, PDO::PARAM_STR);
+$statement->execute();
+$user = $statement->fetch();
 
+if ($user) {
+    $_SESSION['error_message'] = 'すでに登録済みのメールアドレスです';
+    header('Location: ./signup.php');
+    exit();
+}
+
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT); // パスワードをハッシュ化
+$sqlInsertUsers = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+$statement = $pdo->prepare($sqlInsertUsers);
+$statement->bindParam(':name', $name, PDO::PARAM_STR);
+$statement->bindParam(':email', $email, PDO::PARAM_STR);
+$statement->bindParam(':password', $hashedPassword, PDO::PARAM_STR); // ハッシュ化されたパスワードをバインド
+$statement->execute();
+
+header('Location: signin.php');
+exit();
