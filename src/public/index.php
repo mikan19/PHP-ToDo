0524200b61c8628a6c95053d1b92181456b2b42e
mikan19 <mellamo.rina.ty@gmail.com>
2023-06-22 22:48:52 +0900

    fix:edit.phpを修正しました。

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,27 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <title>Document</title>
+  <base href="http://localhost:8080/">
+</head>
+<body>
+  <header>
+    <h2>Todoアプリ</h2>
+    <nav class="nav-top">
+      <ul>
+        <li><a class="nav-menu" href="index.php">ホーム</a></li>
+        <li><a class="nav-menu" href="category/index.php">カテゴリ一覧</a></li>
+        <li><a class="nav-menu" href="user/logout.php">ログアウト</a></li>
+      </ul>
+    </nav>
+  </header>
+</body>
+  <main>
+    <a href="./create.php">タスクを追加</a>
+  </main>
+</body>
